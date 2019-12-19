52b82cd89c4b681ad5c7037bc6ceca0c3640da6a
badu01 <maccacaroemanuele@gmail.com>
2019-12-19 13:16:49 +0100

    Ultimazione della chat privata all' interno della chat pubblica

--- src/Runnables.java
+++ src/Runnables.java
@@ -48,10 +48,36 @@ public class Runnables implements Runnable {
             for(int i = 0; i < listaSocket.size(); i++){
                 if (mySock.equals(listaSocket.get(i)))cont = i;
             }
+            int temp = -1;
 // ciclo di ricezione dal client e invio di risposta
             while (true) {
+                int a = 0;
                 String str = in.readLine();
+                System.out.println(temp);
+                for(int i = 0; i < user.size(); i++){
+                    if (str.equals(user.get(cont) + ": /" + user.get(i))){
+                        stringaOut = new OutputStreamWriter(mySock.getOutputStream());
+                        buffer = new BufferedWriter(stringaOut);
+                        out = new PrintWriter(buffer, true);
+                        out.println("                                        Hai iniziato una chat privata con: " + user.get(i));
+                        temp = i;
+                        a = 1;
+                    }
+                }
+                if(str.equals(user.get(cont) + ": /all")){
+                    stringaOut = new OutputStreamWriter(mySock.getOutputStream());
+                    buffer = new BufferedWriter(stringaOut);
+                    out = new PrintWriter(buffer, true);
+                    out.println("                                        Sei tornato alla chat di gruppo");
+                    temp = -1;
+                }
                 if (str.equals("                                        " + user.get(cont) + " ha abbandonato la chat ")) {
+                    for (int i = 0; i < listaSocket.size(); i++) {
+                        stringaOut = new OutputStreamWriter(listaSocket.get(i).getOutputStream());
+                        buffer = new BufferedWriter(stringaOut);
+                        out = new PrintWriter(buffer, true);
+                        out.println(str);
+                    }
                     listaSocket.remove(mySock);
                     user.remove(nome);
                     System.out.println("Chiudo la connesione con: " + mySock);
@@ -71,7 +97,12 @@ public class Runnables implements Runnable {
                         out = new PrintWriter(buffer, true);
                         if(!mySock.equals(listaSocket.get(i)))out.println(str);
                     }
-                }else{
+                }else if(temp != -1 && a == 0){
+                    stringaOut = new OutputStreamWriter(listaSocket.get(temp).getOutputStream());
+                    buffer = new BufferedWriter(stringaOut);
+                    out = new PrintWriter(buffer, true);
+                    out.println(str);
+                }else if(temp == -1 && a == 0){
                     for (int i = 0; i < listaSocket.size(); i++) {
                         stringaOut = new OutputStreamWriter(listaSocket.get(i).getOutputStream());
                         buffer = new BufferedWriter(stringaOut);
